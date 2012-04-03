f809d80978e38afa14d61ebddfe61603f2df7052
Hindol Adhya <hindol.adhya@gmail.com>
2012-04-03 22:47:01 +0530

    ready_list now schedules higher priority threads first.

--- lib/kernel/list.h
+++ lib/kernel/list.h
@@ -179,3 +179,9 @@ struct list_elem *list_max (struct list *, list_less_func *, void *aux);
 struct list_elem *list_min (struct list *, list_less_func *, void *aux);
 
 #endif /* lib/kernel/list.h */
+
+/* ***DEBUG*** Helper functions to print specific properties of
+    threads in list. */
+
+/* Print thread priorities of all threads in the list. */
+void list_print_thread_prio (struct list *);
